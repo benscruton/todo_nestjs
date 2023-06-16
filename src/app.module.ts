e93771e9330019be76613105951d96510205ab4d
benscruton <benjamin.scruton@gmail.com>
2023-06-15 23:32:47 -0500

    Config and .env loaded

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from "@nestjs/config";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosController } from './todos/todos.controller';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, TodosController],
   providers: [AppService],
 })
